d2aed90f0e5afa9453243518b98802e35227cadf
aryan87701 <singharyan792005@gmail.com>
2024-11-25 01:31:21 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('.nav-link').forEach(link =>{
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        })
+        document.querySelectorAll('.nav-link').forEach(nav =>{nav.classList.remove('clicken')})
+    
+
+link.classList.add('clicken');})
+})
+
+const offmenu = document.querySelector('.nav-links')    
+const hamburger = document.querySelector('#hamburger')
+hamburger.addEventListener("click", () => {
+    console.log('clicked');
+    offmenu.classList.toggle('open');
+    
+})
